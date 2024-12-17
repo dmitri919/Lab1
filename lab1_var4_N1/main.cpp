d9ff230d530a7f73cf3bca312abd65070eb2df9e
dmalg <dmitri.919@gmail.com>
2024-12-17 18:45:25 +0400

    init commmit

---
+++ lab1_var4_N1/main.cpp
@@ -0,0 +1,127 @@
+#include "set.h"
+float my_tools(Set<int> set1, Set<int> set2);
+int main() {
+    int values[] = { 5, 4, 6 };
+    Set<int> set1(values, 3);
+    
+    Set<int> set2;
+    set2 += 4;
+    set2 += 2;
+    set2 += 6;
+
+    float test = my_tools(set1, set2);
+    std::cout << test << std::endl;
+
+    Set<int> set = set1 + set2;
+    
+    Set<int> set_r(10, 1, 11);
+
+    std::cout << "Set1: " << set1 << std::endl;
+    std::cout << "Set2: " << set2 << std::endl;
+    std::cout << "" << std::endl;
+    std::cout << "Set: " << set << std::endl;
+    std::cout << "Set random: " << set_r << std::endl;
+    std::cout<< "\n" << std::endl;
+    std::cout << "Unique elements: " << set.uniqueElements(set_r) << std::endl;
+    std::cout << "Set - Set2: " << set - set2 << std::endl;//вычитание
+    std::cout << "Intersection Set & Set1: " << set.intersection(set1) << std::endl;//пересечение
+    // Попытка удаления несуществующего элемента
+    std::cout << "Set2 - 4: ";
+    try {
+        set2 -= 4;
+    }
+    catch (const std::invalid_argument& e) {
+        std::cerr << e.what() << std::endl;
+    }
+
+    // Пример с типом float
+    float arr3[] = { 1.1, 2.2, 3.3, 4.4, 5.5 };
+    float arr4[] = { 4.4, 5.5, 6.6, 7.7, 8.8 };
+    Set<float> set3(arr3, 5);
+    Set<float> set4(arr4, 5);
+    Set<float> uniqueFloatElements = set3.uniqueElements(set4);
+    std::cout << "Unique float elements: " << uniqueFloatElements << std::endl;
+
+    // Пример с типом std::string
+    std::string arr5[] = { "apple", "banana", "cherry", "date", "fig" };
+    std::string arr6[] = { "date", "fig", "grape", "honeydew", "kiwi" };
+    Set<std::string> set5(arr5, 5);
+    Set<std::string> set6(arr6, 5);
+    Set<std::string> uniqueStringElements = set5.uniqueElements(set6);
+    std::cout << "Unique string elements: " << uniqueStringElements << std::endl;
+
+    // Пример с типом std::pair<int, double>
+    std::pair<int, double> arr7[] = { {1, 1.1}, {2, 2.2}, {3, 3.3}, {4, 4.4}, {5, 5.5} };
+    std::pair<int, double> arr8[] = { {4, 4.4}, {5, 5.5}, {6, 6.6}, {7, 7.7}, {8, 8.8} };
+    Set<std::pair<int, double>> set7(arr7, 5);
+    Set<std::pair<int, double>> set8(arr8, 5);
+    Set<std::pair<int, double>> uniquePairElements = set7.uniqueElements(set8);
+    std::cout << "Unique pair elements: " << uniquePairElements << std::endl;
+
+    Set<float> set10(10, 0, 11);
+    std::cout << "Random float" << set10 << std::endl;
+
+    return 0;
+}
+
+float my_tools(Set<int> set1, Set<int> set2) {
+    Set<int> tmp = set1 + set2;
+    int sum = 0;
+    for (size_t i = 0; i < tmp.give_me_size(); i++) {
+        sum += tmp[i];
+    }
+
+    return sum / float(tmp.give_me_size());
+};
+
+//int main() {
+//    // Пример с типом float
+//    float arr3[] = { 1.1, 2.2, 3.3, 4.4, 5.5 };
+//    float arr4[] = { 4.4, 5.5, 6.6, 7.7, 8.8 };
+//    Set<float> set3(arr3, 5);
+//    Set<float> set4(arr4, 5);
+//    Set<float> uniqueFloatElements = set3.uniqueElements(set4);
+//    std::cout << "Unique float elements: " << uniqueFloatElements << std::endl;
+//
+//    // Операции с float
+//    set3 += 6.6f;
+//    set3 -= 1.1f;
+//    Set<float> unionFloat = set3 + set4;
+//    Set<float> intersectionFloat = set3.intersection(set4);
+//    std::cout << "Union float elements: " << unionFloat << std::endl;
+//    std::cout << "Intersection float elements: " << intersectionFloat << std::endl;
+//
+//    // Пример с типом std::string
+//    std::string arr5[] = { "apple", "banana", "cherry", "date", "fig" };
+//    std::string arr6[] = { "date", "fig", "grape", "honeydew", "kiwi" };
+//    Set<std::string> set5(arr5, 5);
+//    Set<std::string> set6(arr6, 5);
+//    Set<std::string> uniqueStringElements = set5.uniqueElements(set6);
+//    std::cout << "Unique string elements: " << uniqueStringElements << std::endl;
+//
+//    // Операции с std::string
+//    set5 += "grape";
+//    set5 -= "apple";
+//    Set<std::string> unionString = set5 + set6;
+//    Set<std::string> intersectionString = set5.intersection(set6);
+//    std::cout << "Union string elements: " << unionString << std::endl;
+//    std::cout << "Intersection string elements: " << intersectionString << std::endl;
+//
+//    // Пример с типом std::pair<int, double>
+//    std::pair<int, double> arr7[] = { {1, 1.1}, {2, 2.2}, {3, 3.3}, {4, 4.4}, {5, 5.5} };
+//    std::pair<int, double> arr8[] = { {4, 4.4}, {5, 5.5}, {6, 6.6}, {7, 7.7}, {8, 8.8} };
+//    Set<std::pair<int, double>> set7(arr7, 5);
+//    Set<std::pair<int, double>> set8(arr8, 5);
+//    Set<std::pair<int, double>> uniquePairElements = set7.uniqueElements(set8);
+//    std::cout << "Unique pair elements: " << uniquePairElements << std::endl;
+//
+//    // Операции с std::pair<int, double>
+//    set7 += std::make_pair(9, 9.9);
+//    set7 -= std::make_pair(1, 1.1);
+//    Set<std::pair<int, double>> unionPair = set7 + set8;
+//    Set<std::pair<int, double>> intersectionPair = set7.intersection(set8);
+//    std::cout << "Union pair elements: " << unionPair << std::endl;
+//    std::cout << "Intersection pair elements: " << intersectionPair << std::endl;
+//
+//    return 0;
+//}
---
+++ lab1_var4_N1/set.h
@@ -0,0 +1,279 @@
+#include <iostream>
+#include <stdexcept>
+#include <random>
+#include <string>
+#include <utility>
+
+template <typename T>
+class Set {
+private:
+    T* elements;
+    size_t size;
+    size_t capacity;
+
+    void resize();
+    void addUniq(const T& value);
+
+public:
+    Set();
+    Set(const T* values, size_t size);
+    Set(size_t count, T min, T max);
+    Set(const Set& other); // Конструктор копирования
+    Set& operator=(const Set& other); // Оператор присваивания
+    ~Set();
+
+    T operator[] (size_t i) const;
+
+    Set operator+(const Set& other) const;
+    Set operator-(const Set& other) const;
+    Set operator+(const T& value) const;
+    Set& operator+=(const T& value);
+    Set operator-(const T& value) const;
+    Set& operator-=(const T& value);
+    Set intersection(const Set& other) const;
+    bool contains(const T& value) const;
+
+    friend std::ostream& operator<<(std::ostream& os, const Set& set) {
+        for (size_t i = 0; i < set.size; i++) {
+            os << set.elements[i] << "   ";
+        }
+        return os;
+    }
+
+    Set uniqueElements(const Set& other) const;
+
+    size_t give_me_size() {
+        return size;
+    }
+};
+
+//iostream
+// Перегрузка оператора << для std::pair<int, double>
+std::ostream& operator<<(std::ostream& os, const std::pair<int, double>& p) {
+    os << "(" << p.first << ", " << p.second << ")";
+    return os;
+}
+
+
+// Увеличение размера массива в 2 раза
+template<typename T>
+void Set<T>::resize() {
+    capacity = (capacity == 0) ? 1 : capacity * 2;
+    T* newElements = new T[capacity];
+    for (size_t i = 0; i < size; ++i) {
+        newElements[i] = elements[i];
+    }
+    delete[] elements;
+    elements = newElements;
+}
+
+// Добавление элемента
+template<typename T>
+void Set<T>::addUniq(const T& value) {
+    bool Uniqflag = true;
+    for (size_t i = 0; i < size; i++) {
+        if (elements[i] == value) { 
+            Uniqflag = false; 
+            break;
+        }
+    }
+
+    if (Uniqflag) {
+        if (size >= capacity) { resize(); };
+        elements[size] = value;
+        size++;
+    }
+}
+
+template<typename T>
+Set<T>::Set() : size(0), capacity(1) {
+    elements = new T[1];
+}
+
+template<typename T>
+Set<T>::Set(const T* values, size_t _size) : size(0), capacity(_size) {
+    elements = new T[capacity];
+    for (size_t i = 0; i < _size; i++)
+        addUniq(values[i]);
+}
+
+template<typename T>
+Set<T>::Set(size_t count, T min, T max) : size(0), capacity(count) {
+    if (count > abs(max - min) + 1) {
+        throw std::invalid_argument("Size > [min, max)");
+    }
+    elements = new T[capacity];
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    if constexpr (std::is_same_v<T, std::string> || std::is_same_v<T, std::pair<int, double>>) {
+        throw std::invalid_argument("Random generation for this type is not supported");
+    }
+    else {
+        std::uniform_real_distribution<> dis(min, max);
+        for (size_t i = 0; i < count; i++) {
+            addUniq(static_cast<T>(dis(gen)));
+            if (i = size) { i--; };
+        }
+    }
+}
+
+template<typename T>
+Set<T>::Set(const Set& other) : size(other.size), capacity(other.capacity) {
+    elements = new T[capacity];
+    for (size_t i = 0; i < size; ++i) {
+        elements[i] = other.elements[i];
+    }
+}
+
+template<typename T>
+Set<T>& Set<T>::operator=(const Set& other) {
+    if (this == &other) {
+        return *this;
+    }
+    delete[] elements;
+    size = other.size;
+    capacity = other.capacity;
+    elements = new T[capacity];
+    for (size_t i = 0; i < size; ++i) {
+        elements[i] = other.elements[i];
+    }
+
+    return *this;
+}
+
+template<typename T>
+Set<T>::~Set() {
+    delete[] elements;
+}
+
+template<typename T>
+T Set<T>::operator[](size_t i) const {
+    if (i < size) {
+        return elements[i];
+    }
+    else {
+        throw std::out_of_range("Index out of range");
+    }
+}
+
+template<typename T>
+Set<T> Set<T>::operator+(const Set& other) const {
+    Set result;
+    for (size_t i = 0; i < size; ++i) {
+        result += elements[i];
+    }
+    for (size_t i = 0; i < other.size; ++i) {
+        result += other.elements[i];
+    }
+    return result;
+}
+
+template<typename T>
+Set<T> Set<T>::operator+(const T& value) const {
+    Set result = *this;
+    result += value;
+    return result;
+}
+
+template<typename T>
+Set<T>& Set<T>::operator+=(const T& value) {
+    if (!contains(value)) {
+        addUniq(value);
+    }
+    return *this;
+}
+
+template<typename T>
+Set<T>& Set<T>::operator-=(const T& value) {
+    bool found = false;
+    for (size_t i = 0; i < size; ++i) {
+        if (elements[i] == value) {
+            found = true;
+            for (size_t j = i; j < size - 1; ++j) {
+                elements[j] = elements[j + 1];
+            }
+            --size;
+            break;
+        }
+    }
+    if (!found) {
+        throw std::invalid_argument("Element not found in the set");
+    }
+    return *this;
+}
+
+template<typename T>
+Set<T> Set<T>::operator-(const Set& other) const {
+   /* Set result;
+    for (size_t i = 0; i < size; ++i) {
+        if (!other.contains(elements[i])) {
+            result += elements[i];
+        }
+    }
+    if (result.size == 0) {
+        throw std::invalid_argument("No elements to subtract, result is an empty set");
+    } 
+    return result;*/
+    Set result(elements, size);
+    for (size_t i = 0; i < other.size; i++) {
+        result -= elements[i];
+    }
+    if (result.size == 0) {
+        throw std::invalid_argument("No elements to subtract, result is an empty set");
+    }
+    return result;
+}
+
+template<typename T>
+Set<T> Set<T>::operator-(const T& value) const {
+    Set result = *this;
+    try {
+        result -= value;
+    }
+    catch (const std::invalid_argument& e) {
+        throw std::invalid_argument("Element not found in the set for subtraction");
+    }
+    return result;
+}
+
+
+template<typename T>
+Set<T> Set<T>::intersection(const Set& other) const {
+    Set result;
+    for (size_t i = 0; i < size; ++i) {
+        if (other.contains(elements[i])) {
+            result += elements[i];
+        }
+    }
+    return result;
+}
+
+template<typename T>
+bool Set<T>::contains(const T& value) const {
+    if (size == 0) return false; // Проверка на пустой набор
+    size_t left = 0;
+    size_t right = size - 1;
+    
+    for (size_t i = 0; i < size; i++) {
+        if (elements[i] == value) {
+            return true;
+        }
+    }
+    return false;
+}
+
+template<typename T>
+Set<T> Set<T>::uniqueElements(const Set& other) const {
+    Set result;
+    for (size_t i = 0; i < size; ++i) {
+        if (!other.contains(elements[i])) {
+            result += elements[i];
+        }
+    }
+    for (size_t i = 0; i < other.size; ++i) {
+        if (!contains(other.elements[i])) {
+            result += other.elements[i];
+        }
+    }
+    return result;
+}
